a5b5e93fc6a31149098c1d180dc70420af802ba4
Brian Maggi <brianmaggi@gmail.com>
2015-11-30 22:29:17 -0800

    added sponsorship questions.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <p>Organized by <img src="/wp-content/themes/kafkasummit/images/confluent-logo-footer.png" alt="Organized by Confluent"></p>
-        <p>Questions? Contact <a href="mailto:organizers@kafka-summit.org">organizers@kafka-summit.org</a></p>
+        <p>Questions? Interested in sponsoring Kafka Summit? Contact <a href="mailto:organizers@kafka-summit.org">organizers@kafka-summit.org</a></p>
         <p class="copyright">Copyright &copy; Confluent, Inc. 2015. All rights reserved. Apache&reg;, Apache Kafka, Kafka, and the Apache Kafka logo are either registered trademarks or trademarks of the Apache Software Foundation in the United States and/or other countries.</p>
       </div>
     </div>
