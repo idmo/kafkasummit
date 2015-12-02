4c426f3e8a3769df19d250f4fbef8f14066945a0
Brian Maggi <brianmaggi@gmail.com>
2015-12-01 16:51:11 -0800

    Smaller logo

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 		<div class="container">
 			<div class="row">
 			<div class="col-md-11 col-md-offset-1"> 
-				<a href="http://www.kafka-summit.org/index.php"><img src="/wp-content/themes/kafkasummit/images/kafka-summit-2016-logo.png" alt="Kafka Summit 2016 March 12, 2016" class="img-responsive"></a>       
+				<a href="http://www.kafka-summit.org/index.php"><img src="/wp-content/themes/kafkasummit/images/kafka-summit-2016-logo-smaller.png" alt="Kafka Summit 2016 March 12, 2016" class="img-responsive"></a>       
 			</div>
 			</div>        
 		</div>
