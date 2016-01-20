8dddd8eef865748abe752f63a4a128c73cd0356d
Brian Maggi <brianmaggi@gmail.com>
2016-01-20 10:25:43 -0800

    no message

--- header.php
+++ header.php
@@ -10,10 +10,7 @@
         <title><?php wp_title( '| Kafka Summit 2016', true, 'right' ) ?></title>
 		<meta name="author" content="">
 		<link rel="author" href="">
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 		<?php wp_head() ?>
-
     </head>
     <body <?php body_class() ?>>
 		<header id="page-header">
