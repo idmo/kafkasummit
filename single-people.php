e5fea48b24b16eb31ead94ea28777f4cdfbead47
Brian Maggi <brianmaggi@gmail.com>
2016-01-22 08:19:28 -0800

    Update people template to hide empty profiles.

--- single-people.php
+++ single-people.php
@@ -13,8 +13,12 @@
 	<div class="row">
 		<div class="col-md-8">
 			<?php get_template_part('clip', 'single'); ?>
-			<a href="https://www.linkedin.com/in/<?php the_field('linkedin_profile') ?>"><i class="fa fa-linkedin-square fa-lg"></i></a> 
-			<a href="http://www.twitter.com/<?php the_field('twitter_handle') ?>"><?php the_field('twitter_handle') ?></a>			
+			<?php if( get_field('linkedin_profile')): ?>
+				<a href="https://www.linkedin.com/in/<?php the_field('linkedin_profile') ?>"><i class="fa fa-linkedin-square fa-lg"></i></a> 
+			<?php endif; ?>
+			<?php if( get_field('twitter_handle')): ?>
+				<a href="http://www.twitter.com/<?php the_field('twitter_handle') ?>"><?php the_field('twitter_handle') ?></a>
+			<?php endif; ?>
 		</div>
 		<div class="col-md-4">
 			<img class="img-responsive pull-right thumbnail" src="<?php the_field('photo') ?>" alt="<?php the_field('title') ?>"> 
