a9a3546c01d3a4519fadc3ae4f3c856d8c83ae6a
Brian Maggi <brianmaggi@gmail.com>
2016-02-09 16:14:54 -0800

    updated home with program committee.

--- page-home-program.php
+++ page-home-program.php
@@ -55,13 +55,13 @@
   </div>
   <div class="container">
     <div class="row seven-cols">
-      <div class="col-md-1"><a href="/people/neha-narkhede"><img src="/wp-content/themes/kafkasummit/images/neha-narkhede.png" alt="Neha Narkhede" class="img-responsive thumbnail"><p>Neha Narkhede<br>Program Chair</p></a></div>
-      <div class="col-md-1"><a href="/people/tyler-akidau"><img src="/wp-content/themes/kafkasummit/images/tyler-akidau.png" alt="Tyler Akidau" class="img-responsive thumbnail"><p>Tyler Akidau</p></a></div>
-      <div class="col-md-1"><a href="/people/joe-crobak"><img src="/wp-content/themes/kafkasummit/images/joe-crobak.png" alt="Joe Crobak" class="img-responsive thumbnail"><p>Joe Crobak</p></a></div>
-      <div class="col-md-1"><a href="/people/todd-palino"><img src="/wp-content/themes/kafkasummit/images/todd-palino.png" alt="Todd Palino" class="img-responsive thumbnail"><p>Todd Palino</p></a></div>
-      <div class="col-md-1"><a href="/people/jun-rao"><img src="/wp-content/themes/kafkasummit/images/jun-rao.png" alt="Jun Rao" class="img-responsive thumbnail"><p>Jun Rao</p></a></div>
-      <div class="col-md-1"><a href="/people/gwen-shapira"><img src="/wp-content/themes/kafkasummit/images/gwen-shapira.png" alt="Gwen Shapira" class="img-responsive thumbnail"><p>Gwen Shapira</p></a></div>      
-      <div class="col-md-1"><a href="/people/joe-stein"><img src="/wp-content/themes/kafkasummit/images/joe-stein.png" alt="Joe Stein" class="img-responsive thumbnail"><p>Joe Stein</p></a></div>      
+      <div class="col-md-1"><a href="/people/neha-narkhede"><img src="<?php echo get_template_directory_uri() ?>/images/neha-narkhede.png" alt="Neha Narkhede" class="img-responsive thumbnail"><p>Neha Narkhede<br>Program Chair</p></a></div>
+      <div class="col-md-1"><a href="/people/tyler-akidau"><img src="<?php echo get_template_directory_uri() ?>/images/tyler-akidau.png" alt="Tyler Akidau" class="img-responsive thumbnail"><p>Tyler Akidau</p></a></div>
+      <div class="col-md-1"><a href="/people/joe-crobak"><img src="<?php echo get_template_directory_uri() ?>/images/joe-crobak.png" alt="Joe Crobak" class="img-responsive thumbnail"><p>Joe Crobak</p></a></div>
+      <div class="col-md-1"><a href="/people/todd-palino"><img src="<?php echo get_template_directory_uri() ?>/images/todd-palino.png" alt="Todd Palino" class="img-responsive thumbnail"><p>Todd Palino</p></a></div>
+      <div class="col-md-1"><a href="/people/jun-rao"><img src="<?php echo get_template_directory_uri() ?>/images/jun-rao.png" alt="Jun Rao" class="img-responsive thumbnail"><p>Jun Rao</p></a></div>
+      <div class="col-md-1"><a href="/people/gwen-shapira"><img src="<?php echo get_template_directory_uri() ?>/images/gwen-shapira.png" alt="Gwen Shapira" class="img-responsive thumbnail"><p>Gwen Shapira</p></a></div>      
+      <div class="col-md-1"><a href="/people/joe-stein"><img src="<?php echo get_template_directory_uri() ?>/images/joe-stein.png" alt="Joe Stein" class="img-responsive thumbnail"><p>Joe Stein</p></a></div>      
     </div>
   </div>
 </div>
