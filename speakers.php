9ad786bc817210c13a88c602de6c6598f5a45bae
Brian Maggi <brianmaggi@gmail.com>
2016-02-26 08:16:49 -0800

    Updated speaker page, moved location under session title.

--- speakers.php
+++ speakers.php
@@ -33,7 +33,7 @@
 
 					<?php $speaker_sessions = get_field('sessions') ?>
 					<?php foreach ($speaker_sessions as $post) : ?>
-						<div><a href="<?php the_permalink(); ?>"><?php the_title( ); ?></a>, <?php the_field('location'); ?></div>
+						<div><a href="<?php the_permalink(); ?>"><?php the_title( ); ?></a><br><?php the_field('location'); ?></div>
 					<?php endforeach ?>
 			</div>
 		</div>
