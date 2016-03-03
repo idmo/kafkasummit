c91c728ec074dbaa52594245443e4af7ba4386a1
Brian Maggi <brianmaggi@gmail.com>
2016-03-02 16:17:10 -0800

    Updated schedule links.

--- page-schedule.php
+++ page-schedule.php
@@ -47,7 +47,7 @@
 					</tr>
 					<tr>
 						<td class="time">11:40 AM - 12:20 PM</td>
-						<td><span class="session">Advanced Streaming Analytics with Apache Flink and Apache Kafka</span><br> Data Artisans, <a href="http://kafka-summit.org/speakers/stephan-ewen/" class="speaker">Stephan Ewen</a></td>
+						<td><span class="session"><a href="http://kafka-summit.org/sessions/advanced-streaming-analytics-with-apache-flink-and-apache-kafka/">Advanced Streaming Analytics with Apache Flink and Apache Kafka</a></span><br> Data Artisans, <a href="http://kafka-summit.org/speakers/stephan-ewen/" class="speaker">Stephan Ewen</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/when-it-absolutely-positively-has-to-be-there-reliability-guarantees-in-kafka/" class="session">When it absolutely, positively, has to be there: Reliability guarantees in Kafka</a></span><br>Confluent, <a href="http://kafka-summit.org/speakers/gwen-shapira/" class="speaker">Gwen Shapira</a></td>
 						<td><span class="session">Siphon - Near Real Time Databus Using Kafka</span><br>Microsoft, Nitin Kumar, Eric Boyd</td>
 					</tr>
@@ -57,14 +57,14 @@
 					</tr>
 					<tr>
 						<td class="time">1:30 PM - 2:10 PM</td>
-						<td>&nbsp;</td>
-						<td><span class="session">Some “Kafkaesque” days in operations at Linkedin in 2015</span><br>LinkedIn, Joel Koshy</td>
+						<td><span class="session">Kafka Streams</span><br>Confluent, <a href="http://kafka-summit.org/speakers/neha-narkhede/" class="speaker">Neha Narkhede</a></td>
+						<td><span class="session"><a href="http://kafka-summit.org/sessions/some-kafkaesque-days-in-operations-at-linkedin-in-2015/">Some “Kafkaesque” days in operations at Linkedin in 2015</a></span><br>LinkedIn, Joel Koshy</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/the-enterprise-service-bus-is-dead-long-live-the-enterprise-service-bus/" class="session">The enterprise service bus is dead! Long live the enterprise service bus</a></span><br>Heroku, <a href="http://kafka-summit.org/speakers/rimas-silkaitis/" class="speaker">Rimas Silkaitis</a></td>
 					</tr>
 					<tr>
 						<td class="time">2:20 PM - 3:00 PM</td>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
+						<td><span class="session"><a href="http://kafka-summit.org/sessions/kafka-connect-real-time-data-integration-at-scale-with-apache-kafka/">Kafka Connect: Real-time Data Integration at Scale with Apache Kafka</a></span>Confluent, <a href="http://kafka-summit.org/speakers/ewen-cheslack-postava/" class="speaker">Ewen Cheslack-Postava</a></td>
+						<td><span class="session">Securing Kafka</span><br>Confluent, Ben Stopford, Ismael Juma</td>
 						<td><span class="session">Espresso Replication with Kafka</span><br>LinkedIn, Tom Quiggle</td>
 					</tr>
 					<tr>
@@ -73,21 +73,29 @@
 					</tr>
 					<tr>
 						<td class="time">3:20 PM - 4:00 PM</td>
-						<td>&nbsp;</td>
+						<td><span class="session">Apache Spark + Netflix OSS</span><br>IMB Spark Technology Center, Chris Fregley</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/more-datacenters-more-problems/">More Datacenters, More Problems</a></span><br>LinkedIn, <a href="http://kafka-summit.org/speakers/todd-palino/" class="speaker">Todd Palino</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/leveraging-kafka-for-big-data-in-real-time-bidding-analytics-machine-learning-and-campaign-management-for-globally-distributed-data-flows/">Leveraging Kafka for Big Data in Real Time Bidding, Analytics, Machine Learning and Campaign Management for Globally Distributed Data Flows</a></span><br>Tuplejump, <a href="http://kafka-summit.org/speakers/helena-edelson/" class="speaker">Helena Edelson</a></td>
 					</tr>
 					<tr>
 						<td class="time">4:10 PM - 4:50 PM</td>
-						<td><span class="session">A Toolchain for Location Transparency and Data Evolution</span><br>Under Armour, Paul Osman</td>
-						<td></td>
-						<td></td>
+						<td><span class="session"><a href="http://kafka-summit.org/sessions/simplifying-event-streaming-tools-for-location-transparency-and-data-evolution/">Simplifying Event Streaming: Tools for Location Transparency and Data Evolution</a></span><br>Under Armour, <a href="http://kafka-summit.org/speakers/paul-osman/" class="speaker">Paul Osman</a></td>
+						<td><span class="session"><a href="http://kafka-summit.org/sessions/building-an-event-oriented-data-platform-with-kafka/">Building an event-oriented data platform with Kafka</a></span><br>Rocana, <a href="http://kafka-summit.org/speakers/eric-sammer/" class="speaker">Eric Sammer</a></td>
+						<td><span class="session">Lightening Track</span><br>		
+						<p>Towards A Stream Centered Enterprise, Comcast, Gabe Commeau</p>
+						<p>Kafka, the "DialTone for Data": Building a self-service, scalable, streaming analytics system @ HomeAway, Homeaway, Rene Parra</p>
+						<p>Kafka, Killer of Point-to-Point Integrations, Intuit, Lucian Lita</p>
+						</td>
 					</tr>
 					<tr>
 						<td class="time">5:00 PM - 5:40 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/streaming-sql-2/">Streaming SQL</a></span><br>Hortonworks, <a href="http://kafka-summit.org/speakers/julian-hyde-2/" class="speaker">Julian Hyde</a></td>
 						<td><span class="session">Deploying Kafka at Dropbox</span><br>Dropbox, Mark Smith</td>
-						<td>&nbsp;</td>
+						<td><span class="session">Lightening Track</span>
+						<p>Ingesting Complex Healthcare Data with Apache Kafka, Cerner, Micah Whitacre</p>
+						<p>Kafka and Stream processing, Taking Analytics Real-time, Mike Spicer</p>
+						<p>Never at rest - IoT and Data streaming at British Gas Connected Homes,  Paul Makkar</p>
+						</td>
 					</tr>
 					<tr>
 						<td class="time">&nbsp;</td>
