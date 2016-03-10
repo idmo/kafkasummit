df516f5618b78665be9877270647c1cef39adaad
Brian Maggi <brianmaggi@gmail.com>
2016-03-10 12:37:55 -0800

    Updated schedule grid

--- page-schedule.php
+++ page-schedule.php
@@ -31,15 +31,15 @@
 					</tr>
 					<tr>
 						<td class="time">&nbsp;</td>
-						<td colspan="3"><div class="break">Breakfast 7:30 AM - 9:00 AM</div></td>
+						<td colspan="3"><div class="break">Breakfast 7:30 AM - 9:30 AM</div></td>
 					</tr>
 					<tr>
-						<td class="time">9:00 AM - 10:30 AM</td>
+						<td class="time">9:00 AM - 10:15 AM</td>
 						<td colspan="3" id="general-session"><strong>General Session</strong></td>
 					</tr>
 					<tr>
 						<td class="time">&nbsp;</td>
-						<td colspan="3"><div class="break">Break 10:30 AM - 10:50 AM</div></td>
+						<td colspan="3"><div class="break">Break 10:15 AM - 10:30 AM</div></td>
 					</tr>
 					<tr class="tracks">
 						<th>&nbsp;</th>
@@ -48,45 +48,45 @@
 						<th>Users Track</th>
 					</tr>
 					<tr>
-						<td class="time">10:50 AM - 11:30 AM</td>
+						<td class="time">10:30 AM - 11:10 AM</td>
 						<td><span class="session"><a href=" http://kafka-summit.org/sessions/fundamentals-of-stream-processing/ ">Fundamentals of Stream Processing with Apache Beam</a></span><br>Google, <a href="http://kafka-summit.org/speakers/frances-perry/" class="speaker">Frances Perry</a> &amp; <a href="http://kafka-summit.org/speakers/tyler-akidau/" class="speaker">Tyler Akidau</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/real-time-data-cloud-scale/">Kafka at Scale in Cloud</a></span><br>Netflix, <a href="http://kafka-summit.org/speakers/allen-wang/" class="speaker">Allen Wang</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/stream-processing-with-kafka-in-uber/">Stream Processing with Kafka in Uber</a></span><br>Uber, <a href="http://kafka-summit.org/speakers/danny-yuan/" class="speaker">Danny Yuan</a></td>
 					</tr>
 					<tr>
-						<td class="time">11:40 AM - 12:20 PM</td>
+						<td class="time">11:20 AM - 12:00 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/advanced-streaming-analytics-with-apache-flink-and-apache-kafka/">Advanced Streaming Analytics with Apache Flink and Apache Kafka</a></span><br> Data Artisans, <a href="http://kafka-summit.org/speakers/stephan-ewen/" class="speaker">Stephan Ewen</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/when-it-absolutely-positively-has-to-be-there-reliability-guarantees-in-kafka/" class="session">When it Absolutely, Positively, Has to be There: Reliability Guarantees in Kafka</a></span><br>Confluent, <a href="http://kafka-summit.org/speakers/gwen-shapira/" class="speaker">Gwen Shapira</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/siphon-near-real-time-databus-using-kafka/">Siphon - Near Real Time Databus Using Kafka</a></span><br>Microsoft, <a href="http://kafka-summit.org/speakers/nitin-kumar/" class="speaker">Nitin Kumar</a>, <a href="http://kafka-summit.org/speakers/eric-boyd/" class="speaker">Eric Boyd</a></td>
-					</tr>
+					</tr>					
 					<tr>
-						<td class="time">&nbsp;</td>
-						<td colspan="3"><div class="break">Lunch 12:20 PM - 1:30 PM</div></td>
-					</tr>
-					<tr>
-						<td class="time">1:30 PM - 2:10 PM</td>
+						<td class="time">12:10 PM - 12:50 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/introducing-kafka-streams-large-scale-stream-processing-with-kafka/">Introducing Kafka Streams: Large-scale Stream Processing with Kafka</a></span><br>Confluent, <a href="http://kafka-summit.org/speakers/neha-narkhede/" class="speaker">Neha Narkhede</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/some-kafkaesque-days-in-operations-at-linkedin-in-2015/">Some “Kafkaesque” Days in Operations at Linkedin in 2015</a></span><br>LinkedIn, <a href="http://kafka-summit.org/speakers/joel-koshy/" class="speaker">Joel Koshy</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/the-enterprise-service-bus-is-dead-long-live-the-enterprise-service-bus/" class="session">The Enterprise Service Bus is Dead! Long live the Enterprise Service Bus</a></span><br>Heroku, <a href="http://kafka-summit.org/speakers/rimas-silkaitis/" class="speaker">Rimas Silkaitis</a></td>
 					</tr>
 					<tr>
-						<td class="time">2:20 PM - 3:00 PM</td>
+						<td class="time">&nbsp;</td>
+						<td colspan="3"><div class="break">Lunch 12:50 PM - 1:50 PM</div></td>
+					</tr>
+					<tr>
+						<td class="time">1:50 PM - 2:30 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/kafka-connect-real-time-data-integration-at-scale-with-apache-kafka/">Kafka Connect: Real-time Data Integration at Scale with Apache Kafka</a></span><br>Confluent, <a href="http://kafka-summit.org/speakers/ewen-cheslack-postava/" class="speaker">Ewen Cheslack-Postava</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/securing-kafka/">Securing Kafka</a></span><br>Confluent, <a href="http://kafka-summit.org/speakers/ben-stopford/" class="speaker">Ben Stopford</a>, <a href="http://kafka-summit.org/speakers/ismael-juma/" class="speaker">Ismael Juma</a></a></td>
 						<td><!-- <span class="session">Espresso Replication with Kafka</span><br>LinkedIn, Tom Quiggle --></td>
-					</tr>
+					</tr>					
 					<tr>
-						<td class="time">&nbsp;</td>
-						<td colspan="3"><div class="break">Break 3:00 PM - 3:20 PM</div></td>
-					</tr>
-					<tr>
-						<td class="time">3:20 PM - 4:00 PM</td>
+						<td class="time">2:40 PM - 3:20 PM</td>
 						<td><!-- <span class="session">Apache Spark + Netflix OSS</span><br>IMB Spark Technology Center, Chris Fregley --></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/more-datacenters-more-problems/">More Datacenters, More Problems</a></span><br>LinkedIn, <a href="http://kafka-summit.org/speakers/todd-palino/" class="speaker">Todd Palino</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/leveraging-kafka-for-big-data-in-real-time-bidding-analytics-machine-learning-and-campaign-management-for-globally-distributed-data-flows/">Leveraging Kafka for Big Data in Real Time Bidding, Analytics, Machine Learning and Campaign Management for Globally Distributed Data Flows</a></span><br>Tuplejump, <a href="http://kafka-summit.org/speakers/helena-edelson/" class="speaker">Helena Edelson</a></td>
 					</tr>
 					<tr>
-						<td class="time">4:10 PM - 4:50 PM</td>
+						<td class="time">&nbsp;</td>
+						<td colspan="3"><div class="break">Break 3:20 PM - 3:40 PM</div></td>
+					</tr>					
+					<tr>
+						<td class="time">3:40 PM - 4:20 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/simplifying-event-streaming-tools-for-location-transparency-and-data-evolution/">Simplifying Event Streaming: Tools for Location Transparency and Data Evolution</a></span><br>Under Armour, <a href="http://kafka-summit.org/speakers/paul-osman/" class="speaker">Paul Osman</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/building-an-event-oriented-data-platform-with-kafka/">Building an Eevent-oriented Data Platform with Kafka</a></span><br>Rocana, <a href="http://kafka-summit.org/speakers/eric-sammer/" class="speaker">Eric Sammer</a></td>
 						<td><span class="session">Lightning Sessions</span><br>		
@@ -96,7 +96,7 @@
 						</td>
 					</tr>
 					<tr>
-						<td class="time">5:00 PM - 5:40 PM</td>
+						<td class="time">4:30 PM - 5:10 PM</td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/streaming-sql-2/">Streaming SQL</a></span><br>Hortonworks, <a href="http://kafka-summit.org/speakers/julian-hyde-2/" class="speaker">Julian Hyde</a></td>
 						<td><span class="session"><a href="http://kafka-summit.org/sessions/deploying-kafka-at-dropbox/">Deploying Kafka at Dropbox</a></span><br>Dropbox, Mark Smith &amp; <a href="http://kafka-summit.org/speakers/sean-fellows/" class="speaker">Sean Fellows</a></td>
 						<td><span class="session">Lightning Sessions</span>
@@ -107,7 +107,7 @@
 					</tr>
 					<tr>
 						<td class="time">&nbsp;</td>
-						<td colspan="3"><div class="break">Kafka-Palooza Party 5:45 PM - 9:00 PM</div></td>
+						<td colspan="3"><div class="break">Kafka-Palooza Party 6:00 PM - 9:00 PM</div></td>
 					</tr>
 				</table>
 				</div>
