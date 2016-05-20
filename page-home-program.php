d4c7d87fe9c49c99ed9666d51113de9b19d2275a
Brian Maggi <brianmaggi@gmail.com>
2016-05-20 13:22:53 -0700

    Updated home page with broken links

--- page-home-program.php
+++ page-home-program.php
@@ -10,7 +10,13 @@
 <?php get_header(); ?>
 
 
-    <?php get_template_part('loop', 'home'); ?>    
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <?php get_template_part('loop', 'home'); ?>    
+    </div>
+  </div>
+</div>
 	
 <div class="slide" id="masthead">
     <div class="container">
@@ -151,13 +157,13 @@
   </div>
   <div class="container">
     <div class="row seven-cols">
-      <div class="col-md-1"><a href="/people/neha-narkhede"><img src="<?php echo get_template_directory_uri() ?>/images/neha-narkhede.png" alt="Neha Narkhede" class="img-responsive thumbnail"><p>Neha Narkhede<br>Program Chair</p></a></div>
-      <div class="col-md-1"><a href="/people/tyler-akidau"><img src="<?php echo get_template_directory_uri() ?>/images/tyler-akidau.png" alt="Tyler Akidau" class="img-responsive thumbnail"><p>Tyler Akidau</p></a></div>
-      <div class="col-md-1"><a href="/people/joe-crobak"><img src="<?php echo get_template_directory_uri() ?>/images/joe-crobak.png" alt="Joe Crobak" class="img-responsive thumbnail"><p>Joe Crobak</p></a></div>
-      <div class="col-md-1"><a href="/people/todd-palino"><img src="<?php echo get_template_directory_uri() ?>/images/todd-palino.png" alt="Todd Palino" class="img-responsive thumbnail"><p>Todd Palino</p></a></div>
-      <div class="col-md-1"><a href="/people/jun-rao"><img src="<?php echo get_template_directory_uri() ?>/images/jun-rao.png" alt="Jun Rao" class="img-responsive thumbnail"><p>Jun Rao</p></a></div>
-      <div class="col-md-1"><a href="/people/gwen-shapira"><img src="<?php echo get_template_directory_uri() ?>/images/gwen-shapira.png" alt="Gwen Shapira" class="img-responsive thumbnail"><p>Gwen Shapira</p></a></div>      
-      <div class="col-md-1"><a href="/people/joe-stein"><img src="<?php echo get_template_directory_uri() ?>/images/joe-stein.png" alt="Joe Stein" class="img-responsive thumbnail"><p>Joe Stein</p></a></div>      
+      <div class="col-md-1"><a href="/people/neha-narkhede"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/neha-narkhede.png" alt="Neha Narkhede" class="img-responsive thumbnail"><p>Neha Narkhede<br>Program Chair</p></a></div>
+      <div class="col-md-1"><a href="/people/tyler-akidau"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/tyler-akidau.png" alt="Tyler Akidau" class="img-responsive thumbnail"><p>Tyler Akidau</p></a></div>
+      <div class="col-md-1"><a href="/people/joe-crobak"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/joe-crobak.png" alt="Joe Crobak" class="img-responsive thumbnail"><p>Joe Crobak</p></a></div>
+      <div class="col-md-1"><a href="/people/todd-palino"><img src="http://kafka-summit.org/wp-content/uploads/2016/02/todd-palino.png" alt="Todd Palino" class="img-responsive thumbnail"><p>Todd Palino</p></a></div>
+      <div class="col-md-1"><a href="/people/jun-rao"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/jun-rao.png" alt="Jun Rao" class="img-responsive thumbnail"><p>Jun Rao</p></a></div>
+      <div class="col-md-1"><a href="/people/gwen-shapira"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/gwen-shapira.png" alt="Gwen Shapira" class="img-responsive thumbnail"><p>Gwen Shapira</p></a></div>      
+      <div class="col-md-1"><a href="/people/joe-stein"><img src="http://kafka-summit.org/wp-content/uploads/2016/01/joe-stein.png" alt="Joe Stein" class="img-responsive thumbnail"><p>Joe Stein</p></a></div>      
     </div>
   </div>
 </div>
